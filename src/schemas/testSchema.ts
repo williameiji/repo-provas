2cb4da32987933996672d9b26c02a963fae09ead
williameiji <william.fujimori@gmail.com>
2022-09-13 13:09:30 -0300

    feat: route to add tests

--- src/schemas/testSchema.ts
+++ src/schemas/testSchema.ts
@@ -8,19 +8,9 @@ const testSchema = joi.object({
 			/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
 		)
 		.required(),
-	category: joi.any().valid("Projeto", "Prática", "Recuperação").required(),
-	discipline: joi
-		.any()
-		.valid(
-			"HTML e CSS",
-			"JavaScript",
-			"React",
-			"Humildade",
-			"Planejamento",
-			"Autoconfiança"
-		)
-		.required(),
-	teacher: joi.any().valid("Diego Pinho", "Bruna Hamori").required(),
+	category: joi.string().required(),
+	discipline: joi.string().required(),
+	teacher: joi.string().required(),
 });
 
 export default testSchema;
